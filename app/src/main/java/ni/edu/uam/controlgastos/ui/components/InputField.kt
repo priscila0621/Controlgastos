ec381fd7f347459145f0407fc57ef7b4e7ad0a41
Emma Serrano <emmaceciliaserranourroz@gmail.com>
2026-04-15 17:49:26 -0600

    Segundo commit rama emma

---
+++ app/src/main/java/ni/edu/uam/controlgastos/ui/components/InputField.kt
@@ -0,0 +1,24 @@
+package ni.edu.uam.controlgastos.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun InputField(
+    label: String,
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = {
+            Text(text = label)
+        },
+        singleLine = true,
+        modifier = Modifier.fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium
+    )
+}
